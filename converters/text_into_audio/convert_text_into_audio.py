41625dc6dbee3834c0c46b8d1a5d64ca1dbc8701
Aliaksandr Samsonik <aliaksandr_samsonik@epam.com>
2021-08-24 17:03:50 +0300

    Path to the config.yaml has been fixed.

--- converters/text_into_audio/convert_text_into_audio.py
+++ converters/text_into_audio/convert_text_into_audio.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 from yaml import load, FullLoader
 
-with open("config.yaml", "r") as file:
+with open("text_into_audio/config.yaml", "r") as file:
     config = load(file, Loader=FullLoader)
 
 sp = gTTS(text=config.get("text"), lang=config.get("language"), slow=False)
