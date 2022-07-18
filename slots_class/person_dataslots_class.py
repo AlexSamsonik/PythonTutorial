aaaf87b41ee34f6d50e97aefa226146b138b6d2d
Alexander <44325769+AlexSamsonik@users.noreply.github.com>
2022-07-18 12:27:30 +0200

    Slots class (#1)
    * Examples of usage __slots__ have been added.
    
    * Docstring have been added to Slotclass example.
    
    Co-authored-by: Aliaksandr Samsonik <aliaksandr_samsonik@epam.com>

---
+++ slots_class/person_dataslots_class.py
@@ -0,0 +1,54 @@
+"""
+This is provides example of usage __slots__ for increase performance with get, set and delete property on the object.
+"""
+from __future__ import annotations
+
+import timeit
+from dataclasses import dataclass
+from functools import partial
+
+
+@dataclass(slots=False)
+class Person:
+    """There is simple Person data class."""
+    name: str
+    surname: str
+    address: str
+    email: str
+
+
+@dataclass(slots=True)
+class PersonSlots:
+    """There is simple Person class but use __slots__."""
+
+    name: str
+    surname: str
+    address: str
+    email: str
+
+
+def get_set_delete(person: Person | PersonSlots):
+    """
+    Function with set, get and delete address on Person or PersonSlots classes.
+    :param person: Person | PersonSlots object.
+    """
+
+    person.address = "Main St"
+    _ = person.address
+    del person.address
+
+
+def main():
+    """Script which check difference between used and unused __slots__."""
+    person = Person("Dane", "Stone", "Boston", "dane.stone@gmail.com")
+    person_slots = PersonSlots("Dane", "Stone", "Boston", "dane.stone@gmail.com")
+    number = 1_000_000
+    no_slots = min(timeit.repeat(partial(get_set_delete, person), number=number))
+    slots = min(timeit.repeat(partial(get_set_delete, person_slots), number=number))
+    print(f"No slots: {no_slots}")
+    print(f"Slots: {slots}")
+    print(f"Performance improvement: {1 - slots / no_slots:.2%}")
+
+
+if __name__ == '__main__':
+    main()
